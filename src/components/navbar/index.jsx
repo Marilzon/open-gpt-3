97be0a0d9acf5444e2ea2161f6ed497dee3e8fec
Marilzon <maxmaril@hotmail.com>
2022-11-20 18:16:09 -0300

    FEATURE: paste folder in assets/image

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './styles.css'
 
 const Navbar = () => {
   return (
-	<div>
-
-	</div>
+	<nav className='app__navbar'>
+		<div className='app__navbar-links'>
+			<div className="app__navbar-links_logo">
+				<img src={logo} alt="app logo" />
+			</div>
+		</div>
+	</nav>
   )
 }
 
