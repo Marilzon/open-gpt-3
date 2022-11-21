48a82c33ba0b5cf44366da24e1a9f400be406485
Marilzon <maxmaril@hotmail.com>
2022-11-20 22:02:30 -0300

    UPDATE: Desktop navbar completed

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import applicationLogo from '../../assets/images/logo.svg'
 import './styles.css'
 
 const Navbar = () => {
-  return (
-	<nav className='app__navbar'>
-		<div className='app__navbar-links'>
-			<div className="app__navbar-links_logo">
-				<img src={logo} alt="app logo" />
+	return (
+		<nav className='app__navbar'>
+			<div className="app__navbar-container">
+				<figure className="app__navbar-logo">
+					<img src={applicationLogo} alt="gpt-3 svg logo" />
+				</figure>
+
+				<div className="app__navbar-container">
+					<div className="app__navbar-links">
+						<p><a href="#home">Inicio</a></p>
+						<p><a href="#whats-gpt3">O que é GPT-3?</a></p>
+						<p><a href="#open-ai">Open AI</a></p>
+						<p><a href="#features">Recursos</a></p>
+						<p><a href="#blog">Blog</a></p>
+					</div>
+
+					<div className="app__navbar-sign">
+						<p>Sign IN</p>
+						<button type="button">Sign UP</button>
+					</div>
+
+					<div className="app__navbar-dropdown">
+						MENU
+					</div>
+				</div>
 			</div>
-		</div>
-	</nav>
-  )
+
+		</nav>
+	)
 }
 
 export default Navbar
